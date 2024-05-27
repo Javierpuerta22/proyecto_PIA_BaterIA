4367a464c95bf4315617a49ace9bd7cbdfe055a7
Javiphoedata <javip@phoedata.com>
2024-05-27 17:21:29 +0200

    chore: Update .gitignore to include *.pyc and *.pkl files, and update dashboard.py to import necessary libraries and fix function name

--- app/backend/routes/dashboard.py
+++ app/backend/routes/dashboard.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, request, jsonify, current_app
 import pandas as pd
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
 dashboard = Blueprint('dashboard', __name__)
 
 
-def formating_data_to_frontend(df:pd.Dataframe, column: str):
+def formating_data_to_frontend(df:pd.DataFrame, column: str):
     data_formated = {"labels": [x for x in df["t"].values],
                      "datasets": [{"label": column,
                                    "data": [x for x in df[column].values],
@@ -14,15 +17,13 @@ def formating_data_to_frontend(df:pd.Dataframe, column: str):
     
     return data_formated
     
-    
-    
 
 @dashboard.route('/dashboard', methods=['GET'])
 def dashboard_route():
-    
     #tomamos el último csv subido
+    
     #leemos el csv
-    df = pd.read_csv('uploads/ejemplo.csv', sep=',', encoding='utf-8')
+    df = pd.read_csv('uploads/outputs.csv', sep=',', encoding='utf-8')
     
     #hacemos la predicción
     #df['pred'] = model.predict(df)
@@ -30,18 +31,20 @@ def dashboard_route():
     #formateamos los datos para el frontend
     columns = df.columns
     
+    model = pickle.load(open('model.pkl', 'rb'))
+    
+    
     res = {"plots": {}}
     
-    if "id" in columns: 
-        for id_uniq in df["id"].unique():
-            df_id = df[df["id"] == id_uniq]
+    if "Battery_ID" in columns: 
+        for id_uniq in df["Battery_ID"].unique():
+            df_id = df[df["Battery_ID"] == id_uniq]
             res["plots"][id_uniq] = {}
             for column in columns:
-                if column != "id" and column != "pred":
+                if column != "Battery_ID" and column != "pred":
                     data_formated = formating_data_to_frontend(df_id, column)
                     res["plots"][id_uniq][column] = data_formated
-
-    
+                    
     else:
         res["plots"]["1"] = {}
         for column in columns:
